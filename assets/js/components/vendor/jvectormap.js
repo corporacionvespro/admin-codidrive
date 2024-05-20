2518d1eb1ae9f3172cdc423206ae5453738d3cd7
henrydelgadodlc <henrydelgadomar@gmail.com>
2024-05-20 12:41:08 -0500

    menu

--- assets/js/components/vendor/jvectormap.js
+++ assets/js/components/vendor/jvectormap.js
@@ -15,7 +15,7 @@ var VectorMap = (function() {
                 200: '#e9ecef',
                 300: '#dee2e6',
                 400: '#ced4da',
-                500: '#adb5bd',
+                500: '#ffffff00',
                 600: '#8898aa',
                 700: '#525f7f',
                 800: '#32325d',
