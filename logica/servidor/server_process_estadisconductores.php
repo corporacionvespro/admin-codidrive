60837ba38e6beeafe88f1b48fd701d3627106711
henrydelgadodlc <henrydelgadomar@gmail.com>
2024-05-22 17:27:43 -0500

    colores

--- logica/servidor/server_process_estadisconductores.php
+++ logica/servidor/server_process_estadisconductores.php
@@ -215,7 +215,7 @@ while ($aRow = $rResult->fetch_array()) {
 				$row[] = "<td class='text-center'>" . 'S/ ' .  number_format($monto_n, 2, '.', ',') . "</td>";
 			}
 		} elseif ($aColumnas2[$i] == "cantidad_dias") {
-			$row[] = "<td class='text-center'><span style='font-size: 19px;font-weight: bold;color: #01296e;background: #e9e0f5;padding: 2px 10px 2px 10px;border-radius: 25px;'>" . $aRow[$aColumnas2[$i]] . "</span></td>";
+			$row[] = "<td class='text-center'><span style='font-size: 19px;font-weight: bold;color: #01296e;background: #ecf7fd;padding: 2px 10px 2px 10px;border-radius: 25px;'>" . $aRow[$aColumnas2[$i]] . "</span></td>";
 		} elseif ($aColumnas2[$i] == "monto_dia") {
 			$monto_m = $aRow[$aColumnas2[$i]];
 			if ($monto_m == 'NaN') {
