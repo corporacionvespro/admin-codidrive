363ff9c20a69c157feadfed339cf3e6c81810300
henrydelgadodlc <henrydelgadomar@gmail.com>
2024-06-13 14:14:34 -0500

    compartir

--- compartido/index.php
+++ compartido/index.php
@@ -1,68 +1,94 @@
+<!DOCTYPE html>
 <html lang="en">
 
-
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
- <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" type="text/css">
-    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha384-6fAfBF0FBzZ1CVmQnHg4PBhT5lbE2c4XFXOY5FOpWeWifwYRY6DQVXos3KXQc7Bp" crossorigin="anonymous">
+    <style>
+        .header {
+            background: #01296e;
+            display: flex;
+            justify-content: space-between;
+            padding: 12px;
+        }
+
+        .header img {
+            width: 100%;
+        }
+
+        .content {
+            display: flex;
+            align-items: center;
+            padding: 12px;
+        }
+
+        .content img {
+            width: 49px;
+        }
+
+        .info p {
+            margin: 0;
+            padding: 0;
+        }
+
+        .location p {
+            margin: 0;
+            padding: 0;
+        }
+
+        @media (max-width: 576px) {
+            .header {
+                flex-direction: column;
+                align-items: center;
+            }
+
+            .content {
+                flex-direction: column;
+                align-items: center;
+                text-align: center;
+            }
+
+            .location {
+                text-align: center;
+            }
+        }
+    </style>
 </head>
 
 <body>
-    <div class="container" style="background: #01296e !important;display: flex;justify-content: space-between;">
-        <div class="" style="padding: 12px 12px 12px 0px;width: 50px;">
-            <img src="https://codidrive.com/admin/assets/img/brand/logo3.png" class="img-fluid" alt="..." style="
-">
+    <div class="container-fluid header">
+        <div class="logo">
+            <img src="https://codidrive.com/admin/assets/img/brand/logo3.png" class="img-fluid" alt="Logo">
         </div>
-        <div class="d-flex" style="">
-            <div class="" style="padding: 15px 10px 15px 0;">
-                <img src="https://codidrive.com/admin/imagenes/playstore.png" class="img-fluid" alt="..."
-                    style="width: 100px;">
+        <div class="d-flex align-items-center">
+            <div class="me-2">
+                <img src="https://codidrive.com/admin/imagenes/playstore.png" class="img-fluid" alt="Play Store" style="width: 100px;">
             </div>
-            <div class="" style="padding: 15px 0 15px 0;">
-                <img src="https://codidrive.com/admin/imagenes/apple.png" class="img-fluid" alt="..."
-                    style="width: 100px;">
+            <div>
+                <img src="https://codidrive.com/admin/imagenes/apple.png" class="img-fluid" alt="Apple Store" style="width: 100px;">
             </div>
         </div>
     </div>
-    <div class="container" style="display: flex;">
-        <div class="" style="padding: 12px;">
-            <img src="https://codidrive.com/admin/assets/img/brand/logo.png" class="img-fluid" alt="..."
-                style="width: 49px;">
-        </div>
-        <div class=""
-            style="display: flex;justify-content: center;flex-direction: column;flex-wrap: nowrap;align-items: flex-start;">
-            <p style="
-    padding: 0px;
-    margin: 0px;
-">Paulo César 4,87</p>
 
-            <p style="
-    padding: 0px;
-    margin: 0px;
-">plata alto M3L576</p>
+    <div class="container content">
+        <div>
+            <img src="https://codidrive.com/admin/assets/img/brand/logo.png" class="img-fluid" alt="Logo">
         </div>
-    </div>
-    <div class="container" style="display: flex;padding: 0px 23px 18px 23px;">
-        <div class=""
-            style="display: flex;justify-content: flex-start;flex-direction: column;flex-wrap: nowrap;align-items: flex-start;">
-            <p style="
-    padding: 0px;
-    margin: 0px;
-"> <i class="fa-solid fa-location-dot"></i> Urb Santa Rosa</p>
-
-            <p style="
-    padding: 0px;
-    margin: 0px;
-"><i class="fa-solid fa-location-dot"></i> Hospital Regional de Lambayeque (Chiclayo)</p>
+        <div class="info ms-3">
+            <p>Paulo César 4,87</p>
+            <p>plata alto M3L576</p>
         </div>
     </div>
 
+    <div class="container location">
+        <p><i class="fa-solid fa-location-dot"></i> Urb Santa Rosa</p>
+        <p><i class="fa-solid fa-location-dot"></i> Hospital Regional de Lambayeque (Chiclayo)</p>
+    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
