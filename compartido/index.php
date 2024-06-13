4213666dfe3a0ae8caaa6f848f99f65c64f91084
henrydelgadodlc <henrydelgadomar@gmail.com>
2024-06-13 14:22:54 -0500

    compartir

--- compartido/index.php
+++ compartido/index.php
@@ -8,24 +8,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha384-6fAfBF0FBzZ1CVmQnHg4PBhT5lbE2c4XFXOY5FOpWeWifwYRY6DQVXos3KXQc7Bp" crossorigin="anonymous">
     <style>
-        .header {
+        .navbar-custom {
             background: #01296e;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 0.5rem 1rem;
-            height: 56px; /* Altura similar a la de un navbar */
         }
 
-        .header img {
-            height: 100%; /* Hace que las imágenes ocupen toda la altura del navbar */
-            max-height: 100%;
-        }
-
-        .store-images {
-            display: flex;
-            gap: 10px;
-            align-items: center; /* Alinea las imágenes verticalmente en el centro */
+        .navbar-custom .navbar-brand img,
+        .navbar-custom .navbar-nav img {
+            height: 40px;
         }
 
         .content {
@@ -62,19 +51,27 @@
 </head>
 
 <body>
-    <div class="container-fluid header">
-        <div class="logo">
-            <img src="https://codidrive.com/admin/assets/img/brand/logo3.png" class="img-fluid" alt="Logo">
-        </div>
-        <div class="store-images">
-            <div>
-                <img src="https://codidrive.com/admin/imagenes/playstore.png" class="img-fluid" alt="Play Store">
+    <nav class="navbar navbar-expand-lg navbar-custom">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="https://codidrive.com/admin/assets/img/brand/logo3.png" alt="Logo">
+            </a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <img src="https://codidrive.com/admin/imagenes/playstore.png" alt="Play Store">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <img src="https://codidrive.com/admin/imagenes/apple.png" alt="Apple Store">
+                        </a>
+                    </li>
+                </ul>
             </div>
-            <div>
-                <img src="https://codidrive.com/admin/imagenes/apple.png" class="img-fluid" alt="Apple Store">
-            </div>
         </div>
-    </div>
+    </nav>
 
     <div class="container content">
         <div>
