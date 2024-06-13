f2f9206c8a12e78f4e4b40314d08735e0cf7e99b
henrydelgadodlc <henrydelgadomar@gmail.com>
2024-06-13 17:28:29 -0500

    compartir

--- compartido/index.php
+++ compartido/index.php
@@ -102,16 +102,16 @@
             </div>
         </div>
     </div>
-
-    <div id='mapa'></div>
-
-    <div class="container"
+ <div class="container"
         style="display: flex; flex-wrap: nowrap; align-content: flex-start; justify-content: center; align-items: flex-start;">
         <button class="btn btn-primary" id="toggleButton"
             style="background: #e2ecf2;color: #01296e;border: none;border-radius: 0px 0px 50px 50px;height: 50px;width: 100px;">
             <i class="bi bi-chevron-double-up" style="font-size: 25px;font-weight: 200;"></i>
         </button>
     </div>
+    <div id='mapa'></div>
+
+   
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
