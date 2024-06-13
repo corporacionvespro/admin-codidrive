e7596e8f0d7aa3173d724d299f50631e0295fad9
henrydelgadodlc <henrydelgadomar@gmail.com>
2024-06-13 14:17:04 -0500

    compartir

--- compartido/index.php
+++ compartido/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,20 +13,33 @@
             display: flex;
             justify-content: space-between;
             padding: 12px;
+            flex-wrap: wrap;
         }
 
-        .header img {
-            width: 100%;
+        .header .logo,
+        .header .store-images {
+            flex: 1;
+        }
+
+        .store-images {
+            display: flex;
+            justify-content: flex-end;
+            gap: 10px;
         }
 
         .content {
             display: flex;
             align-items: center;
             padding: 12px;
+            flex-wrap: wrap;
         }
 
         .content img {
             width: 49px;
+        }
+
+        .info {
+            margin-left: 12px;
         }
 
         .info p {
@@ -33,36 +47,26 @@
             padding: 0;
         }
 
+        .location {
+            display: flex;
+            padding: 0 23px 18px 23px;
+            flex-wrap: wrap;
+        }
+
         .location p {
             margin: 0;
             padding: 0;
         }
-
-        @media (max-width: 576px) {
-            .header {
-                flex-direction: column;
-                align-items: center;
-            }
-
-            .content {
-                flex-direction: column;
-                align-items: center;
-                text-align: center;
-            }
-
-            .location {
-                text-align: center;
-            }
-        }
     </style>
 </head>
+
 <body>
     <div class="container-fluid header">
         <div class="logo">
             <img src="https://codidrive.com/admin/assets/img/brand/logo3.png" class="img-fluid" alt="Logo">
         </div>
-        <div class="d-flex align-items-center">
-            <div class="me-2">
+        <div class="store-images">
+            <div>
                 <img src="https://codidrive.com/admin/imagenes/playstore.png" class="img-fluid" alt="Play Store" style="width: 100px;">
             </div>
             <div>
@@ -70,18 +74,22 @@
             </div>
         </div>
     </div>
+
     <div class="container content">
         <div>
             <img src="https://codidrive.com/admin/assets/img/brand/logo.png" class="img-fluid" alt="Logo">
         </div>
-        <div class="info ms-3">
+        <div class="info">
             <p>Paulo César 4,87</p>
             <p>plata alto M3L576</p>
         </div>
     </div>
+
     <div class="container location">
-        <p><i class="fa-solid fa-location-dot"></i> Urb Santa Rosa</p>
-        <p><i class="fa-solid fa-location-dot"></i> Hospital Regional de Lambayeque (Chiclayo)</p>
+        <div>
+            <p><i class="fa-solid fa-location-dot"></i> Urb Santa Rosa</p>
+            <p><i class="fa-solid fa-location-dot"></i> Hospital Regional de Lambayeque (Chiclayo)</p>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
