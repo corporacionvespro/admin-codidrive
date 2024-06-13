3d0f295d0894155203b6efd8def2baa3c7c59126
henrydelgadodlc <henrydelgadomar@gmail.com>
2024-06-13 17:43:04 -0500

    compartir

--- compartido/index.php
+++ compartido/index.php
@@ -139,7 +139,7 @@
         </button>
     </div>
 
-    <div id='mapa'></div>
+    <div id='mapa'style="background: #e2ecf2;"></div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
