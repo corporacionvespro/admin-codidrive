7b0e1bc3a47bedf671e657e025484b19ed5007d5
henrydelgadodlc <henrydelgadomar@gmail.com>
2024-06-13 17:01:37 -0500

    contra

--- compartido/index.php
+++ compartido/index.php
@@ -105,12 +105,12 @@
         </button>
     </div>
     <div id='mapa' ></div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
- <script defer 
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCgaYby7k-uwnMsLF3Du3hfHtWpH789Xdo ">
-    </script>
+
    
     <script>
         
@@ -147,5 +147,7 @@
 
         initMap();
    </script>  
-  
+   <script defer 
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCgaYby7k-uwnMsLF3Du3hfHtWpH789Xdo&callback=initMap ">
+    </script>
 </body>
