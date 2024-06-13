1f51ac4dab7f0e1d50fbf0d71485c7527b9c8e2b
henrydelgadodlc <henrydelgadomar@gmail.com>
2024-06-13 16:59:54 -0500

    compartir

--- compartido/index.php
+++ compartido/index.php
@@ -108,7 +108,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
-
+ <script defer 
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCgaYby7k-uwnMsLF3Du3hfHtWpH789Xdo ">
+    </script>
    
     <script>
         
@@ -145,7 +147,5 @@
 
         initMap();
    </script>  
-   <script defer 
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCgaYby7k-uwnMsLF3Du3hfHtWpH789Xdo ">
-    </script>
+  
 </body>
